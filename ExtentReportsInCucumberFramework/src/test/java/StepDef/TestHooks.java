a24580b4340e0012ea11edf9e3b64729910cb2ea
RaghuBN <raghu.astepahead@gmail.com>
2019-11-21 14:58:12 +0530

    Extent Reports in Cucumber

---
+++ ExtentReportsInCucumberFramework/src/test/java/StepDef/TestHooks.java
@@ -0,0 +1,39 @@
+package StepDef;
+
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class TestHooks
+{
+	
+	public static String Current_Scenario_Name = null;
+	
+	//Hooks are nothing but the Pre-Conditions & Post-Conditions
+	//@Before --> Will be executed before each scenario
+	//@After --> Will be executed after each scenario
+	
+	@Before
+	public void beforeScenario(Scenario scenario) 
+	{
+		System.out.println("Started Execution for the Scenario :"+scenario.getName());
+		Current_Scenario_Name = scenario.getName();
+	}
+	
+	//Tagged Hook --> When we are specifying the Pre Conditions for a Particular Scenario
+	@Before("@TestCase2")
+	public void beforeTestCase2(Scenario scenario)
+	{
+		System.out.println("---------------------------------------------------------");
+		System.out.println("Executing Before TestCase2 :"+scenario.getName());
+		System.out.println("---------------------------------------------------------");
+	}
+	
+	@After
+	public void afterScenario(Scenario scenario)
+	{
+		System.out.println("Completed Execution for the Scenario :"+scenario.getName());
+	}
+	
+}
